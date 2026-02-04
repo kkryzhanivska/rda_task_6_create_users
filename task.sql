dbbaef8a22fe24e59bfb6485a7afd541c1c7ad80
Karina Kryzh <krizanivskak@gmail.com>
2026-02-04 21:18:34 +0000

    Fix

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 -- creating webappuser
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.Countries TO 'webappuser'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 -- creating deploymentuser
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
